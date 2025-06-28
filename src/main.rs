aa1fd00e1cc3057cbacf20bc2360788a56e1f0f1
garland-culbreth <culbreth.garland@gmail.com>
2025-06-27 22:45:07 -0700

    check dismod ode

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 pub mod sirrs;
 
 use nalgebra::DVector;
-use sirrs::sir::Model;
-// use sirrs::dismod::Model;
+use sirrs::dismod::Model;
 
 fn main() {
     let mut model: Model = Model {
         length: 365,
-        i_popf_init: 0.01,
-        r_popf_init: 0.0,
-        incidence_rate: 0.04,
-        removal_rate: 0.01,
-        recovery_rate: 0.01,
-        s_popf: DVector::default(),
-        i_popf: DVector::default(),
-        r_popf: DVector::default(),
+        c_init: 0.01,
+        iota: 0.01,
+        rho: 0.01,
+        chi: 0.001,
+        omega: 0.001,
+        s: DVector::default(),
+        c: DVector::default(),
+        rc: DVector::default(),
+        ro: DVector::default(),
     };
     model.init_popf();
     model.run_fdm_o1();
