5eb8290590565b66a9370c55a114f777e66a6c08
garland-culbreth <culbreth.garland@gmail.com>
2025-08-30 19:43:56 -0700

    implement rk4 method
    currently crude but works. subsequent changes will streamline this.

--- src/sirrs/dismod.rs
+++ src/sirrs/dismod.rs
@@ -8,12 +8,14 @@
 //!  - C → Ro
 //!
 //! See [DisMod's latest documentation](https://dismod-at.readthedocs.io/latest/diff_eq.html#diff-eq-title).
-use faer::Mat;
+use faer::{Mat, traits::num_traits::ToPrimitive};
 
 /// Create and run a DisMod-type model.
 pub struct Model {
     /// Number of indices to generate and solve. The length of the series.
     pub length: usize,
+    /// Size of integration step.
+    pub step_size: f64,
     /// Initial with-condition population fraction.
     pub c_init: f64,
     /// Transition rate from S into C. Must be in [0, 1].
@@ -28,10 +30,6 @@ pub struct Model {
     pub s: Mat<f64>,
     /// With-condition population fraction at each index. 1D Array with `length` number of elements.
     pub c: Mat<f64>,
-    /// Removed by condition population fraction at each index. 1D Array with `length` number of elements.
-    pub rc: Mat<f64>,
-    /// Removed by other population fraction at each index. 1D Array with `length` number of elements.
-    pub ro: Mat<f64>,
 }
 
 impl Model {
@@ -39,76 +37,65 @@ impl Model {
     /// to store the population fractions at each index and sets the 0th index
     /// of each equal to the corresponding initial population fraction.
     pub fn init_popf(&mut self) -> &mut Model {
-        self.s = Mat::zeros(self.length, 1);
-        self.c = Mat::zeros(self.length, 1);
-        self.rc = Mat::zeros(self.length, 1);
-        self.ro = Mat::zeros(self.length, 1);
+        let n_steps = (self.length.to_f64().unwrap() / self.step_size)
+            .to_usize()
+            .unwrap();
+        self.s = Mat::zeros(n_steps, 1);
+        self.c = Mat::zeros(n_steps, 1);
         let s_init = 1.0 - self.c_init; // Population fractions must sum to 1.
         self.s[(0, 0)] = s_init;
         self.c[(0, 0)] = self.c_init;
         return self;
     }
 
+    fn ds(&self, s: f64, c: f64) -> f64 {
+        return -((self.iota + self.omega) * s) + (self.rho * c);
+    }
+
+    fn dc(&self, s: f64, c: f64) -> f64 {
+        return (self.iota * s) - ((self.rho + self.chi + self.omega) * c);
+    }
+
     /// Run the DisMod differential equations by the first-order euler method.
     ///
     /// This solution method is very rough and only suitable for demonstration.
     pub fn run_euler(&mut self) -> &Model {
-        for t in 1..self.length {
-            let dsdt = -((self.iota + self.omega) * self.s[(t - 1, 0)]) + (self.rho * self.c[(t - 1, 0)]);
-            let dcdt =
-                (self.iota * self.s[(t - 1, 0)]) - ((self.rho + self.chi + self.omega) * self.c[(t - 1, 0)]);
-            let drcdt = self.chi * self.c[(t - 1, 0)];
-            let drodt = self.omega * (self.s[(t - 1, 0)] + self.c[(t - 1, 0)]);
-            self.s[(t, 0)] = self.s[(t - 1, 0)] + dsdt;
-            self.c[(t, 0)] = self.c[(t - 1, 0)] + dcdt;
-            self.rc[(t, 0)] = self.rc[(t - 1, 0)] + drcdt;
-            self.ro[(t, 0)] = self.ro[(t - 1, 0)] + drodt;
-            println!(
-                "t={}: s={:.6} c={:.6} rc={:.6} ro={:.6}",
-                t, self.s[(t, 0)], self.c[(t, 0)], self.rc[(t, 0)], self.ro[(t, 0)]
-            );
+        let h = self.step_size;
+        let n = (self.length.to_f64().unwrap() / h)
+            .ceil()
+            .to_usize()
+            .unwrap();
+        for t in 1..n - 1 {
+            let dsdt = self.ds(self.s[(t, 0)], self.c[(t, 0)]);
+            let dcdt = self.dc(self.s[(t, 0)], self.c[(t, 0)]);
+            self.s[(t + 1, 0)] = self.s[(t, 0)] + (h * dsdt);
+            self.c[(t + 1, 0)] = self.c[(t, 0)] + (h * dcdt);
+            println!("t={}: s={:.6} c={:.6}", t, self.s[(t, 0)], self.c[(t, 0)],);
         }
         return self;
     }
-    
-    fn ds(&self, susceptible: f64, condition: f64) -> f64 {
-        return -((self.iota + self.omega) * susceptible) + (self.rho * condition);
-    }
 
-    fn dc(&self, susceptible: f64, condition: f64) -> f64 {
-        return (self.iota * susceptible) - ((self.rho + self.chi + self.omega) * condition);
-    }
-
-    fn rk4_step<F>(&self, f: F, x: f64, y: f64, step_size: f64) -> f64
-    where
-        F: Fn(f64, f64) -> f64,
-    {
-        let k1 = step_size * f(x, y);
-        let k2 = step_size * f(x + (step_size / 2.0), y + (k1 / 2.0));
-        let k3 = step_size * f(x + (step_size / 2.0), y + (k2 / 2.0));
-        let k4 = step_size * f(x + step_size, y + k3);
-        let df = (k1 + (2.0 * k2) + (2.0 * k3) + k4) / 6.0;
-        return df;
-    }
-
-    /// Solve the system by the 4th order Runge-Kutta method.
-    ///
-    /// This method is suitable for general purposes.
     pub fn run_rk4(&mut self) -> &Model {
-        let step_size = 0.01;
-        for t in 0..self.length - 1 {
-            let s_t = self.s[(t, 0)];
-            let c_t = self.c[(t, 0)];
-            let dsdt = self.rk4_step(|x, y| self.ds(x, y), s_t, c_t, step_size);
-            let dcdt = self.rk4_step(|x, y| self.dc(x, y), s_t, c_t, step_size);
-            self.s[(t + 1, 0)] = self.s[(t, 0)] + dsdt;
-            self.c[(t + 1, 0)] = self.c[(t, 0)] + dcdt;
-            println!(
-                "t={}: s={:.6} c={:.6}",
-                t,
-                self.s[(t, 0)],
-                self.c[(t, 0)],
-            );
+        let h = self.step_size;
+        let n = (self.length.to_f64().unwrap() / h)
+            .ceil()
+            .to_usize()
+            .unwrap();
+        for i in 0..n - 1 {
+            //
+            let y0 = [self.s[(i, 0)], self.c[(i, 0)]];
+            let k1 = [self.ds(y0[0], y0[1]), self.dc(y0[0], y0[1])];
+            let y1 = [y0[0] + (k1[0] * (h / 2.0)), y0[1] + (k1[1] * (h / 2.0))];
+            let k2 = [self.ds(y1[0], y1[1]), self.dc(y1[0], y1[1])];
+            let y2 = [y0[0] + (k2[0] * (h / 2.0)), y0[1] + (k2[1] * (h / 2.0))];
+            let k3 = [self.ds(y2[0], y2[1]), self.dc(y2[0], y2[1])];
+            let y3 = [y0[0] + (k3[0] * (h)), y0[1] + (k3[1] * h)];
+            let k4 = [self.ds(y3[0], y3[1]), self.dc(y3[0], y3[1])];
+            let dsdt = (k1[0] + (2.0 * k2[0]) + (2.0 * k3[0]) + k4[0]) / 6.0;
+            let dcdt = (k1[1] + (2.0 * k2[1]) + (2.0 * k3[1]) + k4[1]) / 6.0;
+            self.s[(i + 1, 0)] = self.s[(i, 0)] + dsdt;
+            self.c[(i + 1, 0)] = self.c[(i, 0)] + dcdt;
+            println!("t={}: s={:.6} c={:.6}", i, self.s[(i, 0)], self.c[(i, 0)],);
         }
         return self;
     }
@@ -123,6 +110,7 @@ mod tests {
     fn test_init_model() {
         let model: Model = Model {
             length: 10,
+            step_size: 1.0,
             c_init: 0.01,
             iota: 0.01,
             rho: 0.02,
@@ -130,8 +118,6 @@ mod tests {
             omega: 0.04,
             s: Mat::new(),
             c: Mat::new(),
-            ro: Mat::new(),
-            rc: Mat::new(),
         };
         assert_eq!(
             model.length, 10,
@@ -167,24 +153,13 @@ mod tests {
             "Bad c, expected Mat::new() got {:?}",
             model.c,
         );
-        assert_eq!(
-            model.rc,
-            Mat::new(),
-            "Bad rc, expected Mat::new() got {:?}",
-            model.rc,
-        );
-        assert_eq!(
-            model.ro,
-            Mat::new(),
-            "Bad ro, expected Mat::new() got {:?}",
-            model.ro,
-        );
     }
 
     #[test]
     fn test_init_popf() {
         let mut model: Model = Model {
             length: 10,
+            step_size: 1.0,
             c_init: 0.01,
             iota: 0.0,
             rho: 0.02,
@@ -192,8 +167,6 @@ mod tests {
             omega: 0.04,
             s: Mat::new(),
             c: Mat::new(),
-            ro: Mat::new(),
-            rc: Mat::new(),
         };
         model.init_popf();
         assert_eq!(
@@ -211,20 +184,6 @@ mod tests {
             model.c.shape(),
         );
         assert_eq!(
-            model.rc.shape(),
-            (model.length, 1),
-            "Bad rc dimensions, expected {:?} got {:?}.",
-            (model.length, 1),
-            model.rc.shape(),
-        );
-        assert_eq!(
-            model.ro.shape(),
-            (model.length, 1),
-            "Bad ro dimensions, expected {:?} got {:?}.",
-            (model.length, 1),
-            model.ro.shape(),
-        );
-        assert_eq!(
             model.s[(0, 0)],
             1.0 - model.c_init,
             "Bad s[(0, 0)] initialization value, expected {} got {}.",
@@ -232,40 +191,24 @@ mod tests {
             model.s[(0, 0)],
         );
         assert_eq!(
-            model.c[(0, 0)], model.c_init,
+            model.c[(0, 0)],
+            model.c_init,
             "Bad c[(0, 0)] initialization value, expected {} got {}.",
-            model.c_init, model.c[(0, 0)],
-        );
-        assert_eq!(
-            model.ro[(0, 0)], 0.0,
-            "Bad ro[(0, 0)] initialization value, expected {} got {}.",
-            0.0, model.c[(0, 0)],
-        );
-        assert_eq!(
-            model.rc[(0, 0)], 0.0,
-            "Bad rc[(0, 0)] initialization value, expected {} got {}.",
-            0.0, model.c[(0, 0)],
+            model.c_init,
+            model.c[(0, 0)],
         );
         for t in 1..model.length {
             assert_eq!(
-                model.s[(t, 0)], 0.0,
+                model.s[(t, 0)],
+                0.0,
                 "Bad s[t>0] initialization value, expected 0.0 got {}.",
                 model.s[(t, 0)]
             );
             assert_eq!(
-                model.c[(t, 0)], 0.0,
+                model.c[(t, 0)],
+                0.0,
                 "Bad c[t>0] initialization value, expected 0.0 got {}.",
                 model.c[(t, 0)]
-            );
-            assert_eq!(
-                model.rc[(t, 0)], 0.0,
-                "Bad rc[t>0] initialization value, expected 0.0 got {}.",
-                model.rc[(t, 0)]
-            );
-            assert_eq!(
-                model.ro[(t, 0)], 0.0,
-                "Bad ro[t>0] initialization value, expected 0.0 got {}.",
-                model.ro[(t, 0)]
             );
         }
     }
@@ -274,6 +217,7 @@ mod tests {
     fn test_run_euler() {
         let mut model: Model = Model {
             length: 10,
+            step_size: 1.0,
             c_init: 0.01,
             iota: 0.0,
             rho: 0.02,
@@ -281,22 +225,16 @@ mod tests {
             omega: 0.04,
             s: Mat::new(),
             c: Mat::new(),
-            ro: Mat::new(),
-            rc: Mat::new(),
         };
         model.init_popf();
         model.run_euler();
         for t in 1..model.length {
-            let dsdt =
-                -((model.iota + model.omega) * model.s[(t - 1, 0)]) + (model.rho * model.c[(t - 1, 0)]);
+            let dsdt = -((model.iota + model.omega) * model.s[(t - 1, 0)])
+                + (model.rho * model.c[(t - 1, 0)]);
             let dcdt = (model.iota * model.s[(t - 1, 0)])
                 - ((model.rho + model.chi + model.omega) * model.c[(t - 1, 0)]);
-            let drcdt = model.chi * model.c[(t - 1, 0)];
-            let drodt = model.omega * (model.s[(t - 1, 0)] + model.c[(t - 1, 0)]);
             model.s[(t, 0)] = model.s[(t - 1, 0)] + dsdt;
             model.c[(t, 0)] = model.c[(t - 1, 0)] + dcdt;
-            model.rc[(t, 0)] = model.rc[(t - 1, 0)] + drcdt;
-            model.ro[(t, 0)] = model.ro[(t - 1, 0)] + drodt;
             assert!(
                 (model.s[(t, 0)] >= 0.0) & (model.s[(t, 0)] <= 1.0),
                 "s[(t, 0)] not in [0, 1] at time {}, got {}",
@@ -308,18 +246,6 @@ mod tests {
                 "c[(t, 0)] not in [0, 1] at time {}, got {}",
                 t,
                 model.c[(t, 0)]
-            );
-            assert!(
-                (model.rc[(t, 0)] >= 0.0) & (model.rc[(t, 0)] <= 1.0),
-                "rc[(t, 0)] not in [0, 1] at time {}, got {}",
-                t,
-                model.rc[(t, 0)]
-            );
-            assert!(
-                (model.ro[(t, 0)] >= 0.0) & (model.ro[(t, 0)] <= 1.0),
-                "ro[(t, 0)] not in [0, 1] at time {}, got {}",
-                t,
-                model.ro[(t, 0)]
             );
             assert_eq!(
                 model.s[(t, 0)],
@@ -336,22 +262,6 @@ mod tests {
                 t,
                 model.c[(t - 1, 0)] + dcdt,
                 model.c[(t, 0)]
-            );
-            assert_eq!(
-                model.rc[(t, 0)],
-                model.rc[(t - 1, 0)] + drcdt,
-                "Bad rc[(t, 0)] at time {}, expected {} got {}",
-                t,
-                model.rc[(t - 1, 0)] + drcdt,
-                model.rc[(t, 0)]
-            );
-            assert_eq!(
-                model.ro[(t, 0)],
-                model.ro[(t - 1, 0)] + drodt,
-                "Bad ro[(t, 0)] at time {}, expected {} got {}",
-                t,
-                model.ro[(t - 1, 0)] + drodt,
-                model.ro[(t, 0)]
             );
         }
     }
--- tests/test_dismod.rs
+++ tests/test_dismod.rs
@@ -5,6 +5,7 @@ use faer::Mat;
 fn dismod_init_popf() {
     let mut model: Model = Model {
         length: 10,
+        step_size: 1.0,
         c_init: 0.01,
         iota: 0.0,
         rho: 0.02,
@@ -12,8 +13,6 @@ fn dismod_init_popf() {
         omega: 0.04,
         s: Mat::new(),
         c: Mat::new(),
-        ro: Mat::new(),
-        rc: Mat::new(),
     };
     model.init_popf();
     assert_eq!(
@@ -31,20 +30,6 @@ fn dismod_init_popf() {
         model.c.shape(),
     );
     assert_eq!(
-        model.rc.shape(),
-        (model.length, 1),
-        "Bad rc dimensions, expected {:?} got {:?}.",
-        (model.length, 1),
-        model.rc.shape(),
-    );
-    assert_eq!(
-        model.ro.shape(),
-        (model.length, 1),
-        "Bad ro dimensions, expected {:?} got {:?}.",
-        (model.length, 1),
-        model.ro.shape(),
-    );
-    assert_eq!(
         model.s[(0, 0)],
         1.0 - model.c_init,
         "Bad s[(0, 0)] initialization value, expected {} got {}.",
@@ -55,16 +40,6 @@ fn dismod_init_popf() {
         model.c[(0, 0)], model.c_init,
         "Bad c[(0, 0)] initialization value, expected {} got {}.",
         model.c_init, model.c[(0, 0)],
-    );
-    assert_eq!(
-        model.ro[(0, 0)], 0.0,
-        "Bad ro[(0, 0)] initialization value, expected {} got {}.",
-        0.0, model.c[(0, 0)],
-    );
-    assert_eq!(
-        model.rc[(0, 0)], 0.0,
-        "Bad rc[(0, 0)] initialization value, expected {} got {}.",
-        0.0, model.c[(0, 0)],
     );
     for t in 1..model.length {
         assert_eq!(
@@ -77,16 +52,6 @@ fn dismod_init_popf() {
             "Bad c[t>0] initialization value, expected 0.0 got {}.",
             model.c[(t, 0)]
         );
-        assert_eq!(
-            model.rc[(t, 0)], 0.0,
-            "Bad rc[t>0] initialization value, expected 0.0 got {}.",
-            model.rc[(t, 0)]
-        );
-        assert_eq!(
-            model.ro[(t, 0)], 0.0,
-            "Bad ro[t>0] initialization value, expected 0.0 got {}.",
-            model.ro[(t, 0)]
-        );
     }
 }
 
@@ -94,6 +59,7 @@ fn dismod_init_popf() {
 fn dismod_run_fdm_o1() {
     let mut model: Model = Model {
         length: 10,
+        step_size: 1.0,
         c_init: 0.01,
         iota: 0.0,
         rho: 0.02,
@@ -101,8 +67,6 @@ fn dismod_run_fdm_o1() {
         omega: 0.04,
         s: Mat::new(),
         c: Mat::new(),
-        ro: Mat::new(),
-        rc: Mat::new(),
     };
     model.init_popf();
     model.run_euler();
@@ -111,12 +75,8 @@ fn dismod_run_fdm_o1() {
             -((model.iota + model.omega) * model.s[(t - 1, 0)]) + (model.rho * model.c[(t - 1, 0)]);
         let dcdt = (model.iota * model.s[(t - 1, 0)])
             - ((model.rho + model.chi + model.omega) * model.c[(t - 1, 0)]);
-        let drcdt = model.chi * model.c[(t - 1, 0)];
-        let drodt = model.omega * (model.s[(t - 1, 0)] + model.c[(t - 1, 0)]);
         model.s[(t, 0)] = model.s[(t - 1, 0)] + dsdt;
         model.c[(t, 0)] = model.c[(t - 1, 0)] + dcdt;
-        model.rc[(t, 0)] = model.rc[(t - 1, 0)] + drcdt;
-        model.ro[(t, 0)] = model.ro[(t - 1, 0)] + drodt;
         assert!(
             (model.s[(t, 0)] >= 0.0) & (model.s[(t, 0)] <= 1.0),
             "s[(t, 0)] not in [0, 1] at time {}, got {}",
@@ -128,18 +88,6 @@ fn dismod_run_fdm_o1() {
             "c[(t, 0)] not in [0, 1] at time {}, got {}",
             t,
             model.c[(t, 0)]
-        );
-        assert!(
-            (model.rc[(t, 0)] >= 0.0) & (model.rc[(t, 0)] <= 1.0),
-            "rc[(t, 0)] not in [0, 1] at time {}, got {}",
-            t,
-            model.rc[(t, 0)]
-        );
-        assert!(
-            (model.ro[(t, 0)] >= 0.0) & (model.ro[(t, 0)] <= 1.0),
-            "ro[(t, 0)] not in [0, 1] at time {}, got {}",
-            t,
-            model.ro[(t, 0)]
         );
         assert_eq!(
             model.s[(t, 0)],
@@ -156,22 +104,6 @@ fn dismod_run_fdm_o1() {
             t,
             model.c[(t - 1, 0)] + dcdt,
             model.c[(t, 0)]
-        );
-        assert_eq!(
-            model.rc[(t, 0)],
-            model.rc[(t - 1, 0)] + drcdt,
-            "Bad rc[(t, 0)] at time {}, expected {} got {}",
-            t,
-            model.rc[(t - 1, 0)] + drcdt,
-            model.rc[(t, 0)]
-        );
-        assert_eq!(
-            model.ro[(t, 0)],
-            model.ro[(t - 1, 0)] + drodt,
-            "Bad ro[(t, 0)] at time {}, expected {} got {}",
-            t,
-            model.ro[(t - 1, 0)] + drodt,
-            model.ro[(t, 0)]
         );
     }
 }
